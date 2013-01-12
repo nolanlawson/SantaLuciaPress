11eb3584d1cc33e7d93eede626bc829ea2647763
Karen B. Sawyer <kb.sawyer@hotmail.com>
2013-01-12 12:08:39 -0800

    Added images to pringint links

--- js/SiteContent.js
+++ js/SiteContent.js
@@ -9,12 +9,137 @@
         large: 'http://content.comrz.com/AcuCustom/Sitename/DAM/374/Tux.png',
         title: 'The tux penguin!'
     };
-    
+	
+	var image001 = {
+        thumbnail: 'images/001.jpg',
+        large: 'images/L001.jpg',
+        title: 'Here be text'
+    };
+		var image002 = {
+        thumbnail: 'images/002.jpg',
+        large: 'images/L002.jpg',
+        title: 'Here be text'
+    };
+		var image003 = {
+        thumbnail: 'images/003.jpg',
+        large: 'images/L003.jpg',
+        title: 'Here be text'
+    };
+		var image004 = {
+        thumbnail: 'images/004.jpg',
+        large: 'images/L004.jpg',
+        title: 'Here be text'
+    };
+		var image005 = {
+        thumbnail: 'images/005.jpg',
+        large: 'images/L005.jpg',
+        title: 'Here be text'
+    };
+		var image006 = {
+        thumbnail: 'images/006.jpg',
+        large: 'images/L006.jpg',
+        title: 'Here be text'
+    };
+		var image007 = {
+        thumbnail: 'images/007.jpg',
+        large: 'images/L007.jpg',
+        title: 'Here be text'
+    };
+		var image007 = {
+        thumbnail: 'images/007.jpg',
+        large: 'images/L007.jpg',
+        title: 'Here be text'
+    };
+		var image008 = {
+        thumbnail: 'images/008.jpg',
+        large: 'images/L008.jpg',
+        title: 'Here be text'
+    };
+		var image009 = {
+        thumbnail: 'images/009.jpg',
+        large: 'images/L009.jpg',
+        title: 'Here be text'
+    };
+		var image010 = {
+        thumbnail: 'images/010.jpg',
+        large: 'images/L010.jpg',
+        title: 'Here be text'
+    };
+		var image011 = {
+        thumbnail: 'images/011.jpg',
+        large: 'images/L011.jpg',
+        title: 'Here be text'
+    };
+		var image012 = {
+        thumbnail: 'images/012.jpg',
+        large: 'images/L012.jpg',
+        title: 'Here be text'
+    };
+	var image013 = {
+        thumbnail: 'images/013.jpg',
+        large: 'images/L013.jpg',
+        title: 'Here be text'
+    };
+		var image014 = {
+        thumbnail: 'images/014.jpg',
+        large: 'images/L014.jpg',
+        title: 'Here be text'
+    };
+		var image015 = {
+        thumbnail: 'images/015.jpg',
+        large: 'images/L015.jpg',
+        title: 'Here be text'
+    };
+		var image016 = {
+        thumbnail: 'images/016.jpg',
+        large: 'images/L016.jpg',
+        title: 'Here be text'
+    };
+		var image017 = {
+        thumbnail: 'images/017.jpg',
+        large: 'images/L017.jpg',
+        title: 'Here be text'
+    };
+		var image018 = {
+        thumbnail: 'images/018.jpg',
+        large: 'images/L018.jpg',
+        title: 'Here be text'
+    };
+		var image019 = {
+        thumbnail: 'images/019.jpg',
+        large: 'images/L019.jpg',
+        title: 'Here be text'
+    };
+		var image020 = {
+        thumbnail: 'images/020.jpg',
+        large: 'images/L020.jpg',
+        title: 'Here be text'
+    };
+		var image012 = {
+        thumbnail: 'images/021.jpg',
+        large: 'images/L020.jpg',
+        title: 'Here be text'
+    };
+		var image012 = {
+        thumbnail: 'images/022.jpg',
+        large: 'images/L012.jpg',
+        title: 'Here be text'
+    };
+		var image012 = {
+        thumbnail: 'images/023.jpg',
+        large: 'images/L012.jpg',
+        title: 'Here be text'
+    };
+		var image012 = {
+        thumbnail: 'images/024.jpg',
+        large: 'images/L020.jpg',
+        title: 'Here be text'
+    };
     SantaLuciaPress.SiteContent = {
         
-        slideshowImages : {
-            letterpress    : [tux, tux, tux, tux, tux, tux, tux, tux, tux, tux],
-            screenprinting : [tux, tux, tux, tux, tux, tux],
+        slideshowImages : {, 
+            letterpress    : [image001, image002, image003, image004, image005, image006, image007, image008, image009, image010, image011, image012, image013, image014, image015, image016, image017, image018, image019, image020, image021, image022, image023, image024],
+            screenprinting : [image001, image002, image003, image004, image005, image006, image007, image008, image009, image010, image011, image012, image013, image014, image015, image016, image017, image018, image019, image020, image021, image022, image023, image024],
             design         : [tux, tux, tux, tux, tux, tux, tux, tux, tux, tux, tux, tux, tux, tux],
             illustration   : [tux, tux, tux, tux]
 
