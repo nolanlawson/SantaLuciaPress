7a88caa44c93b42bd56661df3c0f0a6049e2c106
Nolan Lawson <nolan.lawson@gmail.com>
2013-01-12 14:55:13 -0800

    remove stub text for slideshow images

--- js/SiteContent.js
+++ js/SiteContent.js
@@ -2,218 +2,211 @@
  * Main definition of Santa Lucia Press site content.
  */
 (function() {
-
-    // TODO: delete me and replace with real images
-    var tux = {
-        thumbnail: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Tux.png/220px-Tux.png',
-        large: 'http://content.comrz.com/AcuCustom/Sitename/DAM/374/Tux.png',
-        title: 'The tux penguin!'
-    };
 	
 	var image001 = {
         thumbnail: 'images/001.jpg',
         large: 'images/L001.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image002 = {
         thumbnail: 'images/002.jpg',
         large: 'images/L002.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image003 = {
         thumbnail: 'images/003.jpg',
         large: 'images/L003.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image004 = {
         thumbnail: 'images/004.jpg',
         large: 'images/L004.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image005 = {
         thumbnail: 'images/005.jpg',
         large: 'images/L005.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image006 = {
         thumbnail: 'images/006.jpg',
         large: 'images/L006.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image007 = {
         thumbnail: 'images/007.jpg',
         large: 'images/L007.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image007 = {
         thumbnail: 'images/007.jpg',
         large: 'images/L007.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image008 = {
         thumbnail: 'images/008.jpg',
         large: 'images/L008.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image009 = {
         thumbnail: 'images/009.jpg',
         large: 'images/L009.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image010 = {
         thumbnail: 'images/010.jpg',
         large: 'images/L010.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image011 = {
         thumbnail: 'images/011.jpg',
         large: 'images/L011.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image012 = {
         thumbnail: 'images/012.jpg',
         large: 'images/L012.jpg',
-        title: 'Here be text'
+        title: ''
     };
 	var image013 = {
         thumbnail: 'images/013.jpg',
         large: 'images/L013.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image014 = {
         thumbnail: 'images/014.jpg',
         large: 'images/L014.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image015 = {
         thumbnail: 'images/015.jpg',
         large: 'images/L015.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image016 = {
         thumbnail: 'images/016.jpg',
         large: 'images/L016.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image017 = {
         thumbnail: 'images/017.jpg',
         large: 'images/L017.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image018 = {
         thumbnail: 'images/018.jpg',
         large: 'images/L018.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image019 = {
         thumbnail: 'images/019.jpg',
         large: 'images/L019.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image020 = {
         thumbnail: 'images/020.jpg',
         large: 'images/L020.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image021 = {
         thumbnail: 'images/021.jpg',
         large: 'images/L021.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image022 = {
         thumbnail: 'images/022.jpg',
         large: 'images/L022.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image023 = {
         thumbnail: 'images/023.jpg',
         large: 'images/L023.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image024 = {
         thumbnail: 'images/024.jpg',
         large: 'images/L024.jpg',
-        title: 'Here be text'
+        title: ''
 	};
 		var image2001 = {
         thumbnail: 'images/2001.jpg',
         large: 'images/L2001.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2002 = {
         thumbnail: 'images/2002.jpg',
         large: 'images/L2002.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2003 = {
         thumbnail: 'images/2003.jpg',
         large: 'images/L2003.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2004 = {
         thumbnail: 'images/2004.jpg',
         large: 'images/L2004.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2005 = {
         thumbnail: 'images/2005.jpg',
         large: 'images/L2005.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2006 = {
         thumbnail: 'images/2006.jpg',
         large: 'images/L2006.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2007 = {
         thumbnail: 'images/2007.jpg',
         large: 'images/L2007.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2008 = {
         thumbnail: 'images/2008.jpg',
         large: 'images/L2008.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2009 = {
         thumbnail: 'images/2009.jpg',
         large: 'images/L2009.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2010 = {
         thumbnail: 'images/2010.jpg',
         large: 'images/L2010.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2011 = {
         thumbnail: 'images/2011.jpg',
         large: 'images/L2011.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2012 = {
         thumbnail: 'images/2012.jpg',
         large: 'images/L2012.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2013 = {
         thumbnail: 'images/2013.jpg',
         large: 'images/L2013.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2014 = {
         thumbnail: 'images/2014.jpg',
         large: 'images/L2014.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2015 = {
         thumbnail: 'images/2015.jpg',
         large: 'images/L2015.jpg',
-        title: 'Here be text'
+        title: ''
     };
 		var image2016 = {
         thumbnail: 'images/2016.jpg',
         large: 'images/L2016.jpg',
-        title: 'Here be text'
+        title: ''
     };
     SantaLuciaPress.SiteContent = {
         
